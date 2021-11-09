b716d8d97f6912f7e6b0be0f07e4714bb2d1b925
Cristian <cristianlucatti@gmail.com>
2021-11-09 09:28:00 -0300

    Clase 1 Formato base

---
+++ index.js
@@ -0,0 +1,35 @@
+var peso = document.querySelectorAll(".red")
+for (var i = 0; i < peso.length; i++) {
+    peso[i].addEventListener("click", function() {
+    alert("Clicked Peso")
+    })
+}
+
+var fuerza = document.querySelectorAll(".green")
+for (var i = 0; i < fuerza.length; i++) {
+    fuerza[i].addEventListener("click", function() {
+    alert("Clicked Fuerza")
+    })
+}
+
+var movimiento = document.querySelectorAll(".yellow")
+for (var i = 0; i < movimiento.length; i++) {
+    movimiento[i].addEventListener("click", function() {
+    alert("Clicked Movimiento")
+    })
+}
+
+var golpe = document.querySelectorAll(".orange")
+for (var i = 0; i < golpe.length; i++) {
+    golpe[i].addEventListener("click", function() {
+    alert("Clicked Golpe")
+    })
+}
+
+var violencia = document.querySelectorAll(".black")
+for (var i = 0; i < violencia.length; i++) {
+    violencia[i].addEventListener("click", function() {
+    alert("Clicked Violencia")
+    })
+}
+
